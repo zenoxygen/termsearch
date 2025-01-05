dda76c06a6a2da85b4019d80b53dbb3f5f1def86
zenoxygen <zenoxygen@protonmail.com>
2025-01-06 01:13:37 +0700

    Simplify initialization

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 mod history;
-mod init;
 mod logger;
 mod search;
 mod ui;
 
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use log::{debug, LevelFilter};
 
+use crate::history::read_zsh_history;
 use crate::logger::Logger;
+use crate::search::{get_frequent_commands, search_commands};
+use crate::ui::TerminalUi;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -43,6 +47,59 @@ enum Command {
     },
 }
 
+/// Initialize termsearch for the current shell.
+pub fn handle_init() -> Result<()> {
+    let zsh_script = include_str!("../termsearch.zsh");
+    println!("{}", zsh_script);
+
+    Ok(())
+}
+
+/// Handle the search command.
+///
+/// # Arguments
+///
+/// * `term`: The search term (optional).
+/// * `max_history`: Maximum number of history entries to read.
+/// * `max_results`: Maximum number of results to display.
+/// * `output_file`: File to write the selected command (optional).
+///
+pub fn handle_search(
+    term: Option<String>,
+    max_history: usize,
+    max_results: usize,
+    output_file: Option<String>,
+) -> Result<()> {
+    // Read ZSH history
+    let history = read_zsh_history(max_history)?;
+    debug!("Read {} history entries", history.len());
+
+    // Initialize UI
+    let mut ui = TerminalUi::new(max_results, history)?;
+
+    // Perform search (display most frequent commands if no term provided)
+    let initial_matches = if let Some(term) = &term {
+        search_commands(term, &ui.history, max_results)
+    } else {
+        get_frequent_commands(&ui.history, max_results)
+    };
+
+    // Display initial results
+    ui.set_initial_results(initial_matches)?;
+
+    // Run the UI and get the selected command
+    if let Some(selected_command) = ui.run(term)? {
+        debug!("Selected command: {}", selected_command);
+        if let Some(output_file) = output_file {
+            debug!("Write command to output file: {}", output_file);
+            let mut file = File::create(&output_file)?;
+            writeln!(file, "commandline\t{}", selected_command)?;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     // Get the home directory
     let home_dir = std::env::var("HOME").expect("HOME environment variable not set");
@@ -72,14 +129,14 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     match args.command {
-        Command::Init => init::handle_init()?,
+        Command::Init => handle_init()?,
         Command::Search {
             term,
             output_file,
             max_history,
             max_results,
         } => {
-            search::handle_search(term, max_history, max_results, output_file)?;
+            handle_search(term, max_history, max_results, output_file)?;
         }
     }
 
--- src/search.rs
+++ src/search.rs
@@ -1,63 +1,14 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Write;
 
-use anyhow::Result;
 use chrono::{DateTime, Utc};
 use log::debug;
 
-use crate::history::{read_zsh_history, CommandEntry};
-use crate::ui::TerminalUi;
+use crate::history::CommandEntry;
 
 /// Weight for recency.
 const RECENCY_WEIGHT: f32 = 0.6;
 /// Weight for frequency.
 const FREQUENCY_WEIGHT: f32 = 0.4;
-
-/// Handle the search command.
-///
-/// # Arguments
-///
-/// * `term`: The search term (optional).
-/// * `max_history`: Maximum number of history entries to read.
-/// * `max_results`: Maximum number of results to display.
-/// * `output_file`: File to write the selected command (optional).
-///
-pub fn handle_search(
-    term: Option<String>,
-    max_history: usize,
-    max_results: usize,
-    output_file: Option<String>,
-) -> Result<()> {
-    // Read ZSH history
-    let history = read_zsh_history(max_history)?;
-    debug!("Read {} history entries", history.len());
-
-    // Initialize UI
-    let mut ui = TerminalUi::new(max_results, history)?;
-
-    // Perform search (display most frequent commands if no term provided)
-    let initial_matches = if let Some(term) = &term {
-        search_commands(term, &ui.history, max_results)
-    } else {
-        get_frequent_commands(&ui.history, max_results)
-    };
-
-    // Display initial results
-    ui.set_initial_results(initial_matches)?;
-
-    // Run the UI and get the selected command
-    if let Some(selected_command) = ui.run(term)? {
-        debug!("Selected command: {}", selected_command);
-        if let Some(output_file) = output_file {
-            debug!("Write command to output file: {}", output_file);
-            let mut file = File::create(&output_file)?;
-            writeln!(file, "commandline\t{}", selected_command)?;
-        }
-    }
-
-    Ok(())
-}
 
 /// Search commands based on a term.
 ///
